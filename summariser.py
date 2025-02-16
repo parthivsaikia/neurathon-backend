c9e846dfaa364016b9efacd95dc2c6090d752317
parthivsaikia <parthivsaikia985@gmail.com>
2025-02-16 19:43:42 +0530

    add the tokeniser

--- summariser.py
+++ summariser.py
@@ -19,3 +19,12 @@ def summarize_text(abstract, intro, conclusion, model_name="facebook/bart-large-
         "### Introduction:\n" + intro + "\n\n"
         "### Conclusion:\n" + conclusion
     )
+
+        # Tokenize and summarize
+    inputs = tokenizer(prompt, return_tensors="pt", max_length=512, truncation=True)
+    inputs = {key: value.to(device) for key, value in inputs.items()}
+
+    summary_ids = model.generate(**inputs, max_length=512, min_length=100, do_sample=False)
+    summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+
+    return "\n".join(textwrap.wrap(summary, width=80))
